5fded26b9e378552e4be82e57f6786e51282e149
Kipkoech24796 <kipkoechcollins8298@gmail.com>
2025-09-25 09:59:44 +0300

    Update assignment 2.c

--- assignment 2.c
+++ assignment 2.c
@@ -1,25 +1,24 @@
-// simple c program 
-/*
-Name: Collins kipkoech 
-Registration number: CT101/G/24796/24
-Description: year one semester one units 
-*/
-
+#include <stdio.h >
 #define pi 3.1416
  
-int main (){ 
-
+int main ()
+{float radius, height, volume, surface_area 
 // prompt for inputs 
 printf ("Enter radius of the cylinder: \n");
-scanf ("&radius");
+scanf ("%f"&radius);
 
 printf ("Enter the height of the cylinder: \n");
-scanf ("&height");
+scanf ("%f"&height);
 
-// calculate volume and surface area 
+// calculate volume
+ volume =pi*radius *radius *height;
 
-printf ("volume of the cylinder=\n");
-printf ("surface area of the cylinder=");
+ //Calculate surface_area 
+ surface_area =2*pi*radius*height +2*radius *radius;
+
+ //display results 
+printf ("volume of the cylinder=%.2f\n",volume);
+printf ("surface area of the cylinder=%.2f",surface_area);
 
 return 0;
 
