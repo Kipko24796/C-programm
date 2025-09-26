e37148237b5b4eceeeef7fe6e42fcd70cdb4caca
Kipkoech24796 <kipkoechcollins8298@gmail.com>
2025-09-26 10:32:55 +0300

    Prompt for height, bankbalance and phonenumber.c

--- Prompt for height, bankbalance and phonenumber.c
+++ Prompt for height, bankbalance and phonenumber.c
@@ -5,20 +5,20 @@ Course:Bcs in computer science
 Registration number:CT101/G/24796/24
 Description:Assignment 
 */
-
+#include <stdio.h>
 int main()
 {
   //prompt for height 
     printf("Enteryourheight \n");
-    scanf("&height:");
+    scanf("%d",&height:);
     
     // prompt for bank balance 
     printf ("Enter your bank balance \n");
-    scanf  ("&bankbalance:");
+    scanf  ("%dl",&bankbalance:);
     
     // prompt for phone number 
     printf ("Enter your phone number \n");
-    scanf  ("&phonenumber:");
+    scanf  ("%dll",&phonenumber:);
     
     return 0;
 }
